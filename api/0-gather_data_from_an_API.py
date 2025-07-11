62f5b9c789331a0b3681f29dfb8ca923f07e9d84
Michelle-anyika <m.anyika@alustudent.com>
2025-07-11 20:31:46 +0000

    backend api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,41 +1,43 @@
 #!/usr/bin/python3
 """
-Fetches and displays a user's TODO list progress from a public API.
+Gather data from API for a given employee ID.
+Usage: ./0-gather_data_from_an_API.py <employee_id>
 """
 
-import requests
+import json
 import sys
+import urllib.request
 
-if __name__ == "__main__":
+
+def main():
     if len(sys.argv) != 2:
-        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
+        print("Usage: {} <employee_id>".format(sys.argv[0]))
         sys.exit(1)
+
+    employee_id = sys.argv[1]
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+    tasks_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(employee_id)
 
     try:
-        employee_id = int(sys.argv[1])
-    except ValueError:
-        print("Employee ID must be an integer")
+        with urllib.request.urlopen(user_url) as response:
+            user_data = json.loads(response.read().decode('utf-8'))
+        with urllib.request.urlopen(tasks_url) as response:
+            tasks_data = json.loads(response.read().decode('utf-8'))
+    except Exception as e:
+        print("Error fetching data:", e)
         sys.exit(1)
 
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-    todos_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(employee_id)
+    name = user_data.get("name")
+    total_tasks = len(tasks_data)
+    done_tasks = [task for task in tasks_data if task.get("completed")]
 
-    user_response = requests.get(user_url)
-    if user_response.status_code != 200:
-        print("User not found")
-        sys.exit(1)
-
-    user_data = user_response.json()
-    employee_name = user_data.get("name")
-
-    todos_response = requests.get(todos_url)
-    todos = todos_response.json()
-
-    done_tasks = [task for task in todos if task.get("completed")]
-    total_tasks = len(todos)
-    number_of_done = len(done_tasks)
-
-    print("Employee {} is done with tasks({}/{}):".format(employee_name, number_of_done, total_tasks))
+    print("Employee {} is done with tasks({}/{}):".format(name,
+                                                          len(done_tasks),
+                                                          total_tasks))
     for task in done_tasks:
         print("\t {}".format(task.get("title")))
+
+
+if __name__ == "__main__":
+    main()
 
